ca045da153bbe5647901defe7384187bf320ec04
sonu-babu-0 <148274148+sonu-babu-0@users.noreply.github.com>
2024-02-29 16:58:06 +0530

    Posterity-1

---
+++ src/App.svelte
@@ -0,0 +1,63 @@
+<script>
+</script>
+
+<main>
+  <div class="video-section" id="video1">
+    <video autoplay muted loop class="video-background">
+      <source src="/src/assets/vid1.mp4" type="video/mp4" />
+    </video>
+    <div class="overlay-text">
+      <h1>Posterity.</h1>
+      <p>(inspired by interstellar)</p>
+    </div>
+  </div>
+
+  <div class="video-section" id="video2">
+    <video autoplay muted loop class="video-background">
+      <source src="/src/assets/vid2.mp4" type="video/mp4" />
+    </video>
+    <div class="overlay-text">Hope.</div>
+  </div>
+
+  <div class="video-section" id="video3">
+    <video autoplay muted loop class="video-background">
+      <source src="/src/assets/vid3.mp4" type="video/mp4" />
+    </video>
+    <div class="overlay-text">
+      "Mankind Was Born On Earth. It Was Never Meant To Die Here."
+    </div>
+  </div>
+
+  <div class="video-section" id="video4">
+    <video autoplay muted loop class="video-background">
+      <source src="/src/assets/vid4.mp4" type="video/mp4" />
+    </video>
+    <div class="overlay-text">
+      "We Used To Look Up At The Sky And Wonder At Our Place In The Stars. Now,
+      We Just Look Down And Worry About Our Place In The Dirt."
+    </div>
+  </div>
+
+  <div class="video-container">
+    <video autoplay muted loop class="video-background">
+      <source src="/src/assets/vid.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+    <a
+      href="https://p1-github-io.vercel.app/"
+      class="centered-link"
+      target="_blank">X</a
+    >
+  </div>
+</main>
+
+<style>
+  @font-face {
+    font-family: "MyCustomFont";
+    src:
+      url("/./assets/font/BAHNSCHRIFT.TTF") format("woff2"),
+      url("/./assets/font/BAHNSCHRIFT 1.TTF") format("woff2");
+    font-weight: normal;
+    font-style: normal;
+  }
+</style>
