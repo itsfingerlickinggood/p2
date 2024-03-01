0b4002f01a322609b00a5e237a5b5f7fe4ca2029
sonu-babu-0 <148274148+sonu-babu-0@users.noreply.github.com>
2024-03-01 17:22:37 +0530

    Updated v0.1

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,6 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
