fd94882d69ea90680dea2042a5dd19cebe26e68b
Sonu Babu <102425248+itsfingerlickinggood@users.noreply.github.com>
2024-03-01 18:04:29 +0530

    Updated App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
 <main>
   <div class="video-section" id="video1">
     <video autoplay muted loop class="video-background">
-      <source src="video\vid1.mp4" type="video/mp4" />
+      <source src="https://www.dropbox.com/scl/fi/d9qon0xu4nws6ju8orftu/vid1.mp4?rlkey=1d6l8ept67a73i875jq5l0xdy&raw=1" type="video/mp4" />
     </video>
     <div class="overlay-text">
       <h1>Posterity.</h1>
@@ -14,14 +14,14 @@
 
   <div class="video-section" id="video2">
     <video autoplay muted loop class="video-background">
-      <source src="video\vid2.mp4" type="video/mp4" />
+      <source src="https://www.dropbox.com/scl/fi/t8657k1771r76m4m9284t/vid2.mp4?rlkey=gajssmq86qcszkjihn8rb1sbm&raw=1" type="video/mp4" />
     </video>
     <div class="overlay-text">Hope.</div>
   </div>
 
   <div class="video-section" id="video3">
     <video autoplay muted loop class="video-background">
-      <source src="video\vid3.mp4" type="video/mp4" />
+      <source src="https://www.dropbox.com/scl/fi/i1nhuh4tsk4pbjixlg385/vid3.mp4?rlkey=e9f6gn6m1x78dl6t6rqea9h4i&raw=1" type="video/mp4" />
     </video>
     <div class="overlay-text">
       "Mankind Was Born On Earth. It Was Never Meant To Die Here."
@@ -30,7 +30,7 @@
 
   <div class="video-section" id="video4">
     <video autoplay muted loop class="video-background">
-      <source src="video\vid4.mp4" type="video/mp4" />
+      <source src="https://www.dropbox.com/scl/fi/m4y2szj239nan46cufff4/vid4.mp4?rlkey=1wnxtj8uju94fkn99le1rpktf&raw=1" type="video/mp4" />
     </video>
     <div class="overlay-text">
       "We Used To Look Up At The Sky And Wonder At Our Place In The Stars. Now,
@@ -40,7 +40,7 @@
 
   <div class="video-container">
     <video autoplay muted loop class="video-background">
-      <source src="video\vid.mp4" type="video/mp4" />
+      <source src="https://www.dropbox.com/scl/fi/waof29z2l5bha5kw93egx/vid.mp4?rlkey=w4ktjryn6oelgbhv3wzalivpb&raw=1" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
     <a
